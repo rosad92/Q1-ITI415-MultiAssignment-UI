0956228ae27f243c4af691295155dbba24615a38
rosad92 <49042220+rosad92@users.noreply.github.com>
2020-05-06 23:16:48 -0400

    Collectables scripts and audio

---
+++ Assets/Collection.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collection : MonoBehaviour
+{
+    public AudioClip collect;
+    public AudioSource audio;
+    
+    // Start is called before the first frame update
+    void Start() {
+        audio = GetComponent<AudioSource>();
+    }
+    // Update is called once per frame
+        void OnTriggerEnter(Collider other) {
+        if (other.gameObject.CompareTag("Pick Up")) {
+            audio.PlayOneShot(collect);
+            other.gameObject.SetActive(false);
+            
+        }
+    }
+
+}
